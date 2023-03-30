b6bdabf8adc2b43b1f334eab02179f1ea21ba4af
Victor-Muriithi <vmurithi10@gmail.com>
2023-03-30 20:44:50 +0300

    to be checked by all members

--- app/src/main/java/com/example/paywave/MainActivity.java
+++ app/src/main/java/com/example/paywave/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.paywave;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,13 +12,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.telecom.Call;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 
 import com.example.paywave.Services.Adapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,17 +42,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
         RecyclerView = findViewById(R.id.recyclerView);
         arrayList = new ArrayList<>();
+        System.out.println(uid+"vuyvy");
+        db.collection("user")
+                .document(uid)
+                .collection("details")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                arrayList.add(new Details(document.get("entity").toString(),document.get("payBill").toString() , document.get("accountNumber").toString(),false ));
+                                System.out.println("here " + document.get("entity").toString());
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                adapter = new Adapter(arrayList, MainActivity.this);
 
-        arrayList.add(new Details("Electricity", "234234234", "+254709876754", false));
-        arrayList.add(new Details("Electricity", "234234234", "+254709876754", false));
-        arrayList.add(new Details("Electricity", "234234234", "+254709876754", false));
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+        System.out.println(arrayList);
         adapter = new Adapter(arrayList, MainActivity.this);
         RecyclerView.setLayoutManager(new LinearLayoutManager(this));
         RecyclerView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
-
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
